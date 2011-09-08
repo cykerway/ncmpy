21929bc86e5c7148abb049eb0bfa545c5b54c3ea
Cyker Way <cykerway@gmail.com>
2011-09-08 10:15:13 +0800

    Rename from mpy to ncmpy to solve PyPI conflict.

--- setup.py
+++ setup.py
@@ -4,19 +4,32 @@ from distutils.core import setup, Extension
 import glob
 
 setup(
-        name = 'mpy', 
-        version = '1.0', 
-        author = 'Cyker Way', 
-        author_email = 'cykerway@gmail.com', 
-        url = 'http://mpy.cykerway.com/', 
-        description = 'A [Python + Curses]-based MPD client', 
-        license = 'GPL3', 
-        requires = ['curses', 'httplib2', 'lxml', 'mpd', 'pyosd'], 
-        packages = ['mpy'], 
-        package_dir = {'mpy':'src'}, 
-        scripts = ['mpy'], 
-        data_files = [
-            ('share/doc/mpy', ['INSTALL', 'README']), 
-            ('share/mpy', glob.glob('share/*')), 
-            ]
-        )
+    name = 'ncmpy', 
+    version = '1.0', 
+    author = 'Cyker Way', 
+    author_email = 'cykerway@gmail.com', 
+    url = 'http://ncmpy.cykerway.com/', 
+    description = 'A [Python + Curses]-based MPD client', 
+    long_description = '''
+        ncmpy - a [Python + Curses]-based MPD client
+        ================================
+
+        ncmpy is an MPD client based on Curses. Besides basic playback control and playlist/database management, it supports song rating, auto lyrics fetching and saving, lyrics highlighting and OSD, and searching by tags.
+    ''', 
+    classifiers = [
+        'Development Status :: 5 - Production/Stable', 
+        'Intended Audience :: End Users/Desktop', 
+        'License :: OSI Approved :: GNU General Public License (GPL)', 
+        'Topic :: Multimedia :: Sound/Audio :: Players', 
+        'Programming Language :: Python :: 2.7',
+        ], 
+    license = 'GPL3', 
+    requires = ['curses', 'httplib2', 'lxml', 'mpd', 'pyosd'], 
+    packages = ['ncmpy'], 
+    package_dir = {'ncmpy':'src'}, 
+    scripts = ['ncmpy'], 
+    data_files = [
+        ('share/doc/ncmpy', ['INSTALL', 'README']), 
+        ('share/ncmpy', glob.glob('share/*')), 
+        ]
+    )
